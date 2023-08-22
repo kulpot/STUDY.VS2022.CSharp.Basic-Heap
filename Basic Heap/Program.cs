fe6ca02e28b798eecb0e8838eb46d16a8d9c61b8
Kulpot <kulpotwahu@gmail.com>
2023-08-22 20:07:31 +0800

    Add project files.

---
+++ Basic Heap/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=lLbKkClhzbU&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=4
+//  about memory -- heap -- allocation algorithm
+
+class Cow
+{
+    int mooCount;
+    //bool butchered;     // bool = 1bytes
+}
+
+class MainClass
+{
+    //static void Goo()
+    //{
+    //    int gooeyInt = 29;
+    //    double gooeyDouble = 43;
+    //}
+    static void Main()
+    {
+        int meInt = 5;
+        Cow c = new Cow();
+
+        // betsy = new Cow();
+        // georgy = new Cow();
+        // clone = betsy;
+        // meInt = 8;
+
+
+
+        //int myInt = 5;
+        //int anotherInt = 10;
+        //Goo();
+        //double meDouble = 98.3;
+        //Goo();
+    }
+}
